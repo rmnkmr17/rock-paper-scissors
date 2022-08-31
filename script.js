1e6119dcb9cdf7529b29578ece533d108fbbf592
rmnkmr17 <111117623+rmnkmr17@users.noreply.github.com>
2022-08-31 11:20:38 +0200

    Finished the game round

--- script.js
+++ script.js
@@ -1,37 +1,104 @@
+// Listing the computer's choices
+
+const choices = [
+    "rock",
+    "scissors",
+    "paper"
+];
+
+// A function that gets computer's choice
+
 function getComputerChoice() {
-    const choices = [
-        "Rock", 
-        "Scissors", 
-        "Paper"
-    ];
-    const random = Math.floor(Math.random() * choices.length);
-    console.log(random, choices[random]);
+    const randChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection){
+// A function that decides the winner
+
+function decideWinner(playerSelection, computerSelection) {
+
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "Computer";
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "Player";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "Computer";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "Computer";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "Player";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "Player";
+    }
+    else {
         return "Draw!";
     }
-    else if (playerSelection === "rock" && computerSelection === "scissors"){
-        return "You win!"
+}
+
+// A function that plays a single round
+
+function playRound(playerSelection, computerSelection) {
+    const result = decideWinner(playerSelection, computerSelection);
+    if (result == "Player") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
-    else if (playerSelection === "rock" && computerSelection === "paper"){
-        return "You lost!"
+    else if (result == "Computer") {
+        return `Computer wins! ${computerSelection} beats ${playerSelection}`;
     }
-    else if (playerSelection === "scissors" && computerSelection === "rock"){
-        return "You lost!"
-    }
-    else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return "You win!"
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock"){
-        return "You win!"
-    }
-    else (playerSelection === "paper" && computerSelection === "scissors"){
-        return "You lost!"
+    else {
+        return "It's a draw!";
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// A function that plays a game of five rounds
+
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+
+        if (decideWinner(playerSelection, computerSelection) == "Player") {
+            scorePlayer++;
+        }
+        else if (decideWinner(playerSelection, computerSelection) == "Computer") {
+            scoreComputer++;
+        }
+    }
+
+    if (scorePlayer > scoreComputer) {
+        console.log("You won!");
+    }
+    else if (scorePlayer < scoreComputer) {
+        console.log("Computer won!");
+    }
+    else {
+        console.log("It's a Draw");
+    }
+}
+    // A function that gets player's choice
+
+    function getPlayerChoice() {
+        let validatedInput = false;
+        while (validatedInput == false) {
+            const randChoice = prompt("Rock Paper Scissors");
+            if (randChoice == null) {
+                continue;
+            }
+            const choiceInLower = randChoice.toLowerCase();
+            if (choices.includes(choiceInLower)) {
+                validatedInput = true;
+                return choiceInLower;
+            }
+        }
+    }
+
+    game();
